9589f439abf58d5d20e7e9f9de687359c8c83005
Mikhail <salazar290720035017@gmail.com>
2025-04-21 21:34:19 +0700

    expand export ignore files

--- export-ignore.php
+++ export-ignore.php
@@ -2,8 +2,26 @@
 
 return [
     '.github/',
+    'tests/',
+    'docs/',
+
+    '.gitignore',
+    '.editorconfig',
+
+    'CODE_OF_CONDUCT.md',
+    'CONTRIBUTING.md',
+
+    'phpcs.xml',
+    '.php-cs-fixer.dist.php',
+
+    'phpstan-baseline.neon',
+    'phpstan.neon.dist',
+
+    'rector.php',
+
     'docker-compose.yaml',
     'docker-compose.yml',
+
     'psalm.xml',
-    'tests/',
+    '.psalm/',
 ];
